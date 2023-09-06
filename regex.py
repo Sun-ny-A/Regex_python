fcf108ea331e57a284e16f0fecad46f1d23d9e02
Sun-ny-A <105686944+Sun-ny-A@users.noreply.github.com>
2023-09-06 09:06:52 -0500

    final edits

--- regex.py
+++ regex.py
@@ -8,16 +8,20 @@ with open('./user_records.txt') as f:
 
 group_pattern = re.compile('^([^Invalid\sdata|Record][A-z]+\s[A-z]+)\W*([0-9]{2})\W*([A-z]+\s[A-z]*)\W*$')
 
+valid_records = 0 #always outside of loop
+invalid_records = 0
+
 for x in data:
-    valid = 0
-    invalid = 0
     result = group_pattern.match(x)
     if result:
+        valid_records += 1
         print(f'name: {result.group(1)}, age: {result.group(2)}, country: {result.group(3)}')
-        valid += 1
     else:
-        invalid += 1
+        invalid_records += 1
         print("invalid record")
+
+print(f'There are {valid_records} valid records.')
+print(f'There are {invalid_records} invalid records.')
         
 
     
